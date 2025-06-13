8f610fcecf2158f5c833545a03f50b442c517206
Mateus <mateus.hamade.dev@gmail.com>
2025-06-13 09:10:59 -0300

    feat: remove session and use database and manager

--- public/index.php
+++ public/index.php
@@ -1,23 +1,25 @@
 <?php
-session_start();
-
+use App\config\Database;
 use App\controllers\Register;
 use App\controllers\Consult;
+use App\models\entities\CitizenManager;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $url = $_GET['url'] ?? '';
 
+$database = new Database();
+$manager = new CitizenManager($database->getDatabase());
 
 switch($url) {    
   case 'social/cadastro':
     $controller = new Register();
-    $controller->index();
+    $controller->index($manager);
     break;
   
   case 'social/consulta':
     $controller = new Consult();
-    $controller->index();
+    $controller->index($manager);
     break;
       
   default:
